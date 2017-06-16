64f5b3e45c8859b71bb676314e0e48fc917b9b0f
Russell Lewis <russell89@gmail.com>
2017-06-16 13:01:56 -0400

    Update Import-vCenter-CA-Certificates.ps1

--- PowerShell/Import-vCenter-CA-Certificates.ps1
+++ PowerShell/Import-vCenter-CA-Certificates.ps1
@@ -1,14 +1,5 @@
-
-
-
-
-################################################################
-#          Do not modify anything under this line			   #
-################################################################
-
-
 #Remove downloaded certs if they were downloaded previously
-Remove-Item $PSScriptRoot\certs -Force -Recurse 
+Remove-Item $PSScriptRoot\vcsa-certs -Force -Recurse | Out-Null
 
 #Function for unzipping
 Add-Type -AssemblyName System.IO.Compression.FileSystem
@@ -30,8 +21,8 @@ $adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
 # Check to see if we are currently running "as Administrator"
 if ($myWindowsPrincipal.IsInRole($adminRole))
    {
-   # If session is elevated or user is admin, then continue running.
-   $Host.UI.RawUI.WindowTitle = "Install vCenter Self Signed Certs - (Elevated)"
+   # If session is elevated or user is an admin, then continue running.
+   $Host.UI.RawUI.WindowTitle = "Import vCenter CA Certificates - (Elevated)"
    $Host.UI.RawUI.ForegroundColor = "Green" 
    Clear-Host
    }
@@ -44,31 +35,41 @@ else
    Exit
    }
 
-$vCenter = Read-Host -Prompt 'Enter your vCenter FQDN or ip address'   
+$vCenter = Read-Host -Prompt 'Enter your vCenter FQDN or IP address'   
+Write-Host ""
 $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 #Download Certs
-Write-Host "Downloading CA certificates"
+Write-Host "Attempting to download CA certificates from vCenter"
 $url = "https://$vCenter/certs/download"
 $output = "$PSScriptRoot\certs.zip"
 [Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 $webClient = new-object System.Net.WebClient
-try{$webClient.DownloadFile( $url, $output )}
-catch{write-host "download failed"}
+Try{$webClient.DownloadFile( $url, $output )}
+Catch{
+Write-Warning -Message "Download failed! Please check vCenter FQDN or IP and try again." 
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Exit
+}
 
 #Unzip certificates
 Write-Host "Unzipping CA certificates"
-Unzip "$PSScriptRoot\certs.zip" "$PSScriptRoot"
+Try{Unzip "$PSScriptRoot\certs.zip" "$PSScriptRoot\vcsa-certs"}
+Catch{
+Write-Warning -Message "Unable to extract certificates, please ensure they downloaded properly and try again." 
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Exit
+}
 
 #Install Certs 
-$certs = Get-ChildItem "$PSScriptRoot\certs"
+$certs = Get-ChildItem "$PSScriptRoot\vcsa-certs\certs"
 ForEach ($cert in $certs) { 
-$ca = "$PSScriptRoot\certs\$cert"
+$ca = "$PSScriptRoot\vcsa-certs\$cert"
 Write-Output "Importing downloaded vCenter certificates into Trusted Root Store"
-	try{
+	Try{
 		$ImportError = certutil -addstore -enterprise -f -v root "$ca"
 	}
-	catch{
+	Catch{
 		Write-Output "certutil failed to import certificate: $ImportError"
 	}
 }
